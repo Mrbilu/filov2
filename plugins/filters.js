948f0a3aee89c80d0e5113e8ff39d2791cc28a7e
Tearless <90409549+tearffpc@users.noreply.github.com>
2021-09-17 19:22:00 +0530

    Update filters.js

--- plugins/filters.js
+++ plugins/filters.js
@@ -177,10 +177,10 @@ Asena.addCommand({on: 'text', fromMe: false}, (async (message, match) => {
     if(Config.STICKERP){
     let banned = jid.find( Jid => Jid === message.jid);
     if(banned !== undefined) return
-    if (!!message.mention && message.mention[0] == '918129624395@s.whatsapp.net') {
-await message.client.sendMessage(message.jid, fs.readFileSync('./stickers/Mention.webp'), MessageType.sticker, { mimetype: Mimetype.webp, quoted : message.data, ptt: false})
+    if (!!message.mention && message.mention[0] == '919562881715@s.whatsapp.net') {
+await message.client.sendMessage(message.jid, fs.readFileSync('./stickers/tear.webp'), MessageType.sticker, { mimetype: Mimetype.webp, quoted : message.data, ptt: false})
     }
-const array = ['tear pc','nw akhil','Filo','nexusnw']
+const array = ['bilal ser','mr bilu','tear','tear pc','nw akhil','Filo','nexusnw']
 array.map( async (a) => {
 let pattern = new RegExp(`\\b${a}\\b`, 'g');
 if(pattern.test(message.message)){
